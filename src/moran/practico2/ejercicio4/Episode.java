dbdb07cbea8eb29dc6e8e927ac1bb7b0f6d8386f
Mattii97 <Morandeiramatias@gmail.com>
2019-04-05 16:11:33 -0300

    Ejercicio4 se añadio y testeo

---
+++ src/moran/practico2/ejercicio4/Episode.java
@@ -0,0 +1,43 @@
+package moran.practico2.ejercicio4;
+
+public class Episode {
+    final int minRate=1,maxRate=5;
+    private boolean viewed=false;
+    private int rate=-1;
+    private String title,description;
+
+    public Episode(String title,String description){
+        this.title=title;
+        this.description=description;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    public int getRate(){
+        return rate;
+    }
+
+    public boolean getViewed(){
+        return viewed;
+    }
+
+    public void setViewed(boolean viewed){//modelar asi o hacer uno que ponga true siempre
+        this.viewed=viewed;
+    }
+
+    public boolean setRate(Integer rate){
+        if(this.viewed)
+            if(rate>=minRate && rate<=maxRate){
+                this.rate=rate;
+                return true;
+            }
+        return false;
+    }
+
+}
---
+++ src/moran/practico2/ejercicio4/Serie.java
@@ -0,0 +1,81 @@
+package moran.practico2.ejercicio4;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Serie {
+    private List<Season> seasonList;
+    private String title,description,creator,genre;
+
+    public Serie(){
+        this.seasonList=new ArrayList<Season>();
+    }
+
+    public void addSeason(Season season){
+        seasonList.add(season);
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    public String getCreator(){
+        return creator;
+    }
+
+    public String getGenre(){
+        return creator;
+    }
+
+    public void setTitle(String title){
+        this.title=title;
+    }
+
+    public void setDescription(String description){
+        this.description=description;
+    }
+
+    public void setCreator(String creator){
+        this.creator=creator;
+    }
+
+    public void setGenre(String genre){
+        this.genre=genre;
+    }
+
+    public Double getAverageRate(){
+        double total=0;
+        int numberOfEpisodes=this.getEpisodesView();
+        for(Season s:seasonList){
+            double b = s.getEpisodesViewed();
+            double c = numberOfEpisodes;
+            double a = b/c;
+            total+=a*s.getAverageRate();
+        }
+        return total;
+    }
+
+    public int getNumberOfEpisodes(){
+        int amount=0;
+        for(Season s:seasonList){
+            amount+=s.getNumberOfEpisodes();
+        }
+        return amount;
+    }
+
+    public int getEpisodesView(){
+        int amount=0;
+        for(Season s:seasonList){
+            amount+=s.getEpisodesViewed();
+        }
+        return amount;
+    }
+
+    public boolean seenAll(){
+        return getEpisodesView()==getNumberOfEpisodes();
+    }
+}
