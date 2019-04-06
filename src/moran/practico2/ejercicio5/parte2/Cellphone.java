8df4c6cff13c2eb0654bb6da9ebd41c196572c62
Mattii97 <morandeiramatias@gmail.com>
2019-04-06 13:03:11 -0300

    se empezo codear la clase Cellphone, falta agregar el promedio de edades

--- src/moran/practico2/ejercicio5/parte2/Cellphone.java
+++ src/moran/practico2/ejercicio5/parte2/Cellphone.java
@@ -1,16 +1,58 @@
 package moran.practico2.ejercicio5.parte2;
 
-import java.util.List;
+import java.util.*;
 
 public class Cellphone {
     private List<Contact> contactList;
 
-    public Cellphone(){
-
+    public Cellphone() {
+        contactList = new ArrayList<Contact>();
     }
 
-    public void addContact(){
-
+    public void addContact(Contact contact) {
+        this.contactList.add(contact);
     }
 
+    public int getNumberOfContacts() {
+        return contactList.size();
+    }
+
+    public ArrayList<Contact> getContactList() {
+        return new ArrayList<Contact>(this.contactList);
+    }
+
+    public ArrayList<Contact> getDuplicates() {//como hacer una solucion un poco mas eficiente, esto me parece muy malo.
+        ArrayList<Contact> copy=new ArrayList<Contact>(this.contactList);
+        ArrayList<Contact> reapeted=new ArrayList<Contact>();
+        for(int i=0;i<copy.size();i++)
+            for(int j=i+1;j<copy.size();j++) {
+                Contact inJ=copy.get(j);
+                if(copy.get(i).equals(inJ)){
+                    if(!findContact(reapeted,inJ))
+                        reapeted.add(inJ);
+                    copy.remove(i);
+                }
+            }
+        return reapeted;
+    }
+
+    /*public Set<Contact> findDuplicates() {//porque esto no funciona???? (tiene que ver con algo de hashes me parece)
+
+        final Set<Contact> setToReturn = new HashSet<Contact>();
+        final Set<Contact> set1 = new HashSet<Contact>();
+
+        for (Contact yourInt : this.contactList) {
+            System.out.println(yourInt);
+            if (!set1.add(yourInt)) {
+                setToReturn.add(yourInt);
+            }
+        }
+        return setToReturn;
+    }*/
+    private boolean findContact(List<Contact> list, Contact toFind) {
+        for (Contact c : list)
+            if (toFind.equals(c))
+                return true;
+        return false;
+    }
 }
--- src/moran/practico2/ejercicio5/parte2/Ejercicio5parte2.java
+++ src/moran/practico2/ejercicio5/parte2/Ejercicio5parte2.java
@@ -1,7 +1,10 @@
 package moran.practico2.ejercicio5.parte2;
 
+import java.sql.SQLOutput;
 import java.time.LocalDate;
 import java.time.Month;
+import java.util.ArrayList;
+import java.util.Set;
 
 public class Ejercicio5parte2 {
     public static void main(String[] args) {
@@ -9,7 +12,31 @@ public class Ejercicio5parte2 {
                 LocalDate.of(1994, Month.AUGUST,6),2266482865l);
         Contact juan=new Contact("Juan Pablo","Correa","A la vuelta de la barrial","Villa aguirre","juancorrea@alumnos.exa.unicen.edu.ar",
                 LocalDate.of(1994, Month.AUGUST,6),2494529051l);
-        System.out.println(cabeza);
-        System.out.println(juan);
+        Contact gian=new Contact("Gian","Grecco","al lado del macro","Tandil","giangrecco@alumnos.exa.unicen.edu.ar",
+                LocalDate.of(1994, Month.AUGUST,6),2494529051l);
+        Contact ale=new Contact("Ismael","Aguiar","Saavedra 92","Balcarce","cabeza@gmail.com",
+                LocalDate.of(1994, Month.AUGUST,6),2266482865l);
+        Contact pela=new Contact("Eliel Arturo","Catauro","barrio mirage","Tandil","pela3@alumnos.exa.unicen.edu.ar",
+                LocalDate.of(1994, Month.AUGUST,6),2494529051l);
+        Contact laucha=new Contact("Lautaro","Rockefeller","batidepto","Tandil","lauchaman@alumnos.exa.unicen.edu.ar",
+                LocalDate.of(1994, Month.AUGUST,6),2494529051l);
+        Cellphone oneplus3t=new Cellphone();
+        oneplus3t.addContact(cabeza);
+        oneplus3t.addContact(juan);
+        oneplus3t.addContact(gian);
+        oneplus3t.addContact(ale);
+        oneplus3t.addContact(pela);
+        oneplus3t.addContact(laucha);
+        oneplus3t.addContact(laucha);
+        if(ale.equals(cabeza))
+            System.out.println("son iguales");
+        else
+            System.out.println("SON DISTNTOS");
+
+        ArrayList<Contact> repeated=oneplus3t.getDuplicates();
+        System.out.println(repeated.size());
+        for(Contact c:repeated){
+            System.out.println(c);
+        }
     }
 }
